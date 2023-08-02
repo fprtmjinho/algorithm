d53a8ce727056a1c3abf4fe33991dd6d67967820
fprtmjinho <fjrtmjinho@gmail.com>
2023-08-02 21:56:07 +0900

    23/08/02

--- 1461.cpp
+++ 1461.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int main(){
-    
+    int n, m;
+    int arr[10001] = { 0, };
+    int index = 0;
+    int result = 0;
+    cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] < 0)
+			index++;
+	}
+
+	sort(arr, arr + n);
+	
+	for (int i = n - 1; i >= index; i -= m) {
+			result += (arr[i]*2);
+	}
+
+	for (int i = 0; i < index; i += m) {
+			result += abs(arr[i] * 2);
+	}
+
+	result -= max(abs(arr[0]), abs(arr[n - 1]));
+
+	cout << result << endl;
 }
